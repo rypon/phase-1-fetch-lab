27ca6985c2d0d689b8fd966f20b7851cbda98f4b
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-19 17:02:53 -0500

    commit tree a0d128d9da9b92b45e8d481feddaaef8af28b014

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const booksUrl = "https://anapioficeandfire.com/api/books"
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(booksUrl)
+    .then(resp => resp.json())
+    .then(books => renderBooks(books)
+    );
+
 }
 
 function renderBooks(books) {
@@ -12,6 +18,10 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
+
+
+
+//1) renders book titles into the DOM by passing a JSON object to renderBooks()
